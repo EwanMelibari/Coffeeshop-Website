098670d990ccb311eed19724da80a4b580a40a78
Ewan Melibari <ewanhasan2004@gmail.com>
2025-12-25 18:32:27 +0300

    Adding first endpoint

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {
+        "message": "Welcome to Coffee Shop API",
+        "docs": "/docs",
+        "version": "1.0.0"
+    }
